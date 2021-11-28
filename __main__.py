632a73e9432b16aab647c01fe5c33302b27c4c50
ChancelorReese <Chancelor.Reese@gmail.com>
2021-11-27 17:43:14 -0700

    Had to change to new repository realizing i was writing code in batter program.

---
+++ __main__.py
@@ -0,0 +1,58 @@
+import os
+os.environ['RAYLIB_BIN_PATH'] = '.'
+
+import raylibpy
+import random
+from game import constants
+from game.director import Director
+from game.actor import Actor
+from game.point import Point
+from game.draw_actors_action import DrawActorsAction
+from game.input_service import InputService
+from game.output_service import OutputService
+from game.physics_service import PhysicsService
+#from game.audio_service import AudioService
+
+from game.solid_blocks import SolidBlock
+
+def main():
+    cast = {}
+
+    cast["solid_blocks"] = []
+    solid_blocks = []
+    for row in range(6):
+        for column in range(8):
+            solid_block = SolidBlock(row,column)
+            solid_blocks.append(solid_block)
+    cast["solid_blocks"] = solid_blocks
+    
+    cast["power_ups"] = []
+    cast["blocks"] = []
+    cast["players"] = []
+
+
+    script = {}
+
+    input_service = InputService()
+    output_service = OutputService()
+    physics_service = PhysicsService()
+    # audio_service = AudioService()
+
+    draw_actors_action = DrawActorsAction(output_service)
+
+    script["input"] = []
+    script["update"] = []
+    script["output"] = [draw_actors_action]
+    
+    output_service.open_window("Playing with Fire")
+    #audio_service.start_audio()
+    #audio_service.play_sound(constants.SOUND_START)
+    
+    director = Director(cast, script)
+    director.start_game()
+
+    #audio_service.stop_audio()
+
+if __name__ == "__main__":
+    main()
+
---
+++ playing_with_fire/game/solid_blocks.py
@@ -0,0 +1,20 @@
+from game import constants
+from game.actor import Actor
+from game.point import Point
+
+class SolidBlock(Actor):
+    def __init__(self,row,column):
+        super().__init__()
+        self.row = row
+        self.column = column
+        self.start_block()
+        
+    def start_block(self):
+        self.set_height(constants.BLOCK_HEIGHT)
+        self.set_width(constants.BLOCK_WIDTH)
+        
+        x = 50 + ((self.get_width() + 50) * self.column)
+        y = 50 + ((self.get_height() + 50) * self.row)
+        
+        point = Point(x,y)
+        self.set_position(point)
